226836e9179a45b10f749f43ad0bacb759cb1cce
Atharva Sawant <84018291+Satharva2004@users.noreply.github.com>
2025-04-09 16:16:32 +0530

    Valid Anagram.java

--- Valid Anagram.java
+++ Valid Anagram.java
@@ -16,3 +16,24 @@ class Solution {
         return true;
     }
 }
+
+//using hashMap
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        //s = "racecar", t = "carrace"
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i),0)+1);
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i),0)-1);
+        }
+        for(int n: map.values()){
+            if(n != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
