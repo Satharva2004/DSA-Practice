e09d2a0b65721a096dc498c4fd0e9986eecd61b5
Atharva Sawant <84018291+Satharva2004@users.noreply.github.com>
2025-04-09 21:32:31 +0530

    Top K Frequent Elements.java

--- Top K Frequent Elements.java
+++ Top K Frequent Elements.java
@@ -26,3 +26,29 @@ class Solution {
         return arr;
     }
 }
+
+//Tried using array but understood the concept of bucket sort basically array overrides the new elements with same frequency but bucket sort converts the list into an ArrayList which can store multiple elements at single index
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int n:nums){
+            map.put(n, map.getOrDefault(n,0)+1);
+        }
+        int arr[] = new int[nums.length+1];
+        for(int key: map.keySet()){
+            int frequency = map.get(key);
+            arr[frequency] = key;
+        } 
+        int result[] = new int[k];
+        int index = 0;
+        for(int i=arr.length-1;i>=0 && index < k;i--){
+            if(arr[i] != 0){
+                result[index] = arr[i];
+                index++;
+            }
+        }
+        return result;
+    }
+}
+
