9fb91aa5c87d2e2948ee8408795b325635be4e50
Atharva Sawant <84018291+Satharva2004@users.noreply.github.com>
2025-04-22 16:37:12 +0530

    Update Product of Array Except Self .java

--- Product of Array Except Self .java
+++ Product of Array Except Self .java
@@ -1,3 +1,25 @@
+//using prefix and postfix basically using an pointer that runs from i=0 to the index i and other i+1 to nums.lenght
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        int result[] = new int[len];
+        for(int i=0;i<nums.length;i++){
+            int left = 1;
+            int right = 1;
+            for(int left_index = 0; left_index<i;left_index++){
+                left = left * nums[left_index];
+            }
+            for(int right_index = i+1; right_index<nums.length;right_index++){
+                right = right * nums[right_index];
+            }
+            result[i] = left*right;
+        }
+        return result;
+    }
+}  
+
+
+
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int len = nums.length;
