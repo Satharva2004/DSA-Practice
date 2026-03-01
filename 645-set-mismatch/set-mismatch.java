f8b4003eddabd787fe042327c61eb492455a3686
Atharva Sawant <84018291+Satharva2004@users.noreply.github.com>
2026-03-02 05:09:17 +0530

    Time: 14 ms (32.83%) | Memory: 47.9 MB (24.37%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int ans[] = new int[2];
+        for(int i = 0; i < nums.length; i++){
+            if(set.contains(nums[i])){
+                ans[0] = nums[i];
+            }
+            set.add(nums[i]);
+        }
+        for(int i = 1; i <= nums.length; i++){
+            if(!set.contains(i)){
+                ans[1] = i;
+            }
+        }
+        return ans;
+    }
+}
