0c41bf047106752bebd777524b6ff5a4218dc264
Atharva Sawant <84018291+Satharva2004@users.noreply.github.com>
2026-03-19 23:56:28 +0530

    Time: 1 ms (99.89%) | Memory: 46.6 MB (52.71%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -11,36 +11,23 @@
 import java.math.BigInteger;
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        StringBuilder num1 = new StringBuilder("");
-        StringBuilder num2 = new StringBuilder("");
-        while(l1 != null){
-            num1.append(l1.val);
-            l1 = l1.next;
+        ListNode result = new ListNode(0);
+        ListNode dummy = result;
+        int carry = 0;
+        while(l1 != null || l2 != null || carry != 0){
+            int sum = carry;
+            if(l1 != null){
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            carry = sum/10;
+            dummy.next = new ListNode(sum%10);
+            dummy = dummy.next;
         }
-        num1 = num1.reverse();
-        while(l2 != null){
-            num2.append(l2.val);
-            l2 = l2.next;
-        }
-        num2 = num2.reverse();
-        BigInteger n1 = num1.length() == 0 ? BigInteger.ZERO : new BigInteger(num1.toString());
-        BigInteger n2 = num2.length() == 0 ? BigInteger.ZERO : new BigInteger(num2.toString());
-        BigInteger res = n1.add(n2);
-        if(res.equals(BigInteger.ZERO)) return new ListNode(0);
-        Stack<Integer> result  = new Stack<>();
-        while(res.compareTo(BigInteger.ZERO) > 0){
-            result.push(res.mod(BigInteger.TEN).intValue());
-            res = res.divide(BigInteger.TEN);
-        }
-
-        ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
-
-        while (!result.isEmpty()) {
-            ListNode node = new ListNode(result.pop());
-            node.next = curr.next;
-            curr.next = node;
-        }
-        return dummy.next;
+        return result.next;
     }
 }
