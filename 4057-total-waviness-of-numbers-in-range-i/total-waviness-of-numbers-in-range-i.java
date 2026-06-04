07263e44e8b31b3ced31089055eb9511b36fa468
Atharva Sawant <84018291+Satharva2004@users.noreply.github.com>
2026-06-04 22:16:32 +0530

    Time: 26 ms (72.86%) | Memory: 46.6 MB (20.45%) - LitCode

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
@@ -1,15 +1,21 @@
 class Solution {
+    int count = 0;
     public int totalWaviness(int num1, int num2) {
-        int count = 0;
         for(int i = num1; i <= num2; i++){
-            String sb = Integer.toString(i);
-            for (int j = 1; j < sb.length() - 1; j++) {
-                int prev = sb.charAt(j - 1) - '0';
-                int curr = sb.charAt(j) - '0';
-                int next = sb.charAt(j + 1) - '0';
-                if(curr > prev && curr > next || curr < prev && curr < next){
-                    count++;
-                }
+           generator(i);
+        }
+        return count;
+    }
+    public int generator(int i)
+    {
+        String sb = Integer.toString(i);
+        for (int j = 1; j < sb.length() - 1; j++) 
+        {
+            int prev = sb.charAt(j - 1) - '0';
+            int curr = sb.charAt(j) - '0';
+            int next = sb.charAt(j + 1) - '0';
+            if(curr > prev && curr > next || curr < prev && curr < next){
+                count++;
             }
         }
         return count;
