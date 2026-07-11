81e7bee93b24ae727432ae409bd5b510fab0b00f
Atharva Sawant <84018291+Satharva2004@users.noreply.github.com>
2026-07-11 15:19:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (14.07%) - LitCode

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0||x==1){
+            return x;
+        }
+        long long start=1;
+        long long end =x;
+        long long ans=0;
+
+        while(start<=end){
+           long long mid= start +(end-start)/2;
+            if(mid*mid==x){
+                return mid;
+            }
+            else if(mid*mid<x){
+                ans=mid;
+                start=mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
+};
