c4d2cbccbfc007330157886e0797efe27a8e6b07
Atharva Sawant <84018291+Satharva2004@users.noreply.github.com>
2026-07-13 01:09:32 +0530

    Time: 2 ms (72.89%) | Memory: 47.9 MB (38.62%) - LitCode

---
+++ 130-surrounded-regions/surrounded-regions.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void solve(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, board[0].length - 1);
+        }
+
+        for (int j = 0; j < board[0].length; j++) {
+            dfs(board, 0, j);
+            dfs(board, board.length - 1, j);
+        }
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                if(board[i][j] == 'O'){
+                    board[i][j] = 'X';
+                }else if(board[i][j] == 'T'){
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+    public void dfs(char[][] board, int i, int j){
+        if(i<0 || i>=board.length || j<0 || j>=board[0].length || board[i][j] != 'O'){
+            return;
+        }
+        board[i][j] = 'T';
+        dfs(board, i+1, j);
+        dfs(board, i-1, j);
+        dfs(board, i, j+1);
+        dfs(board, i, j-1);
+    }
+}
