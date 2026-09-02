0762b5ca5148b1a101517f7e6a75e2a23173c820
Atharva Sawant <84018291+Satharva2004@users.noreply.github.com>
2026-09-03 01:44:36 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (73.66%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,20 +1,28 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int l = 0;
-        int r = s.length()-1;
-        while(l < r){
-            if(!Character.isLetterOrDigit(s.charAt(l))){
-                l++;
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            char ch = s.charAt(i);
+            char ch2 = s.charAt(j);
+            if ((ch < 'A' || ch > 'Z') && (ch < 'a' || ch > 'z') && (ch < '0' || ch > '9')){
+                i++;
+                continue;
             }
-            else if(!Character.isLetterOrDigit(s.charAt(r))){
-                r--;
+            if ((ch2 < 'A' || ch2 > 'Z') && (ch2 < 'a' || ch2 > 'z')&& (ch2 < '0' || ch2 > '9')){
+                j--;
+                continue;
             }
-            else {
-                if(Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) return false;
-                l++;
-                r--;
+            char lowerCh = ch;
+            if (lowerCh >= 'A' && lowerCh <= 'Z') {
+                lowerCh = (char) (lowerCh + 32);
             }
-            
+            char lowerCh2 = ch2;
+            if (lowerCh2 >= 'A' && lowerCh2 <= 'Z') {
+                lowerCh2 = (char) (lowerCh2 + 32);
+            }
+            if(lowerCh != lowerCh2) return false;
+            j--;
+            i++;
         }
         return true;
     }
